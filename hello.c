bb2bf69b77a4d14a73f10e4990edbbe331b792ec
Cholo75 <54493034+Cholo75@users.noreply.github.com>
2019-08-25 09:29:11 +0200

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+// get_string and printf with %s
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
